128908e5914d2ccfa9ffc8cc42c2e7af8f6c2c02
Matheus Matos <matheusmatos.221@hotmail.com>
2021-08-17 15:32:54 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# CookieClicker
+Developed by: Matheus Matos
+
+E-mail: matheusmatos.221@hotmail.com
+
+"This is a bot for autoclick in cookieclicker game."
---
+++ main.py
@@ -0,0 +1,19 @@
+import mouse,keyboard, time
+
+def loop(delay):
+    """ Sends a click with the given button and """
+
+    print("Press Ctrl-C to terminate while statement")
+    while True:
+        mouse.click()
+        time.sleep(delay)
+
+
+if __name__ == '__main__':
+    print('Cookie Clicker Bot inicializando em 3...\n')
+    time.sleep(1)
+    print('Cookie Clicker Bot inicializando em 2...\n')
+    time.sleep(1)
+    print('Cookie Clicker Bot inicializando em 1...\n')
+    time.sleep(1)
+    loop(1)
