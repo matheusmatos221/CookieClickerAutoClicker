1e9a1d86d1ecb97ffa0b9c603f1b881f5ba6bb19
Matheus Matos <matheusmatos.221@hotmail.com>
2021-08-18 10:50:40 -0300

    Functional version

---
+++ main.py
@@ -0,0 +1,16 @@
+import mouse,keyboard, time, pyautogui as py
+
+def loop(delay):
+    """ Sends a click with the given button and """
+    x, y = py.position()
+    while True:
+        mouse.click()
+        time.sleep(delay)
+        if x != py.position()[0] or y != py.position()[1]:
+            break
+
+
+if __name__ == '__main__':
+    print('Cookie Clicker Bot inicializando em 1...\n')
+    time.sleep(1)
+    loop(0.004)
